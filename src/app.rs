11908a1bb4f51e8d29d0ab6ce67fc5fc684cb85d
Felix Häcker <haeckerfelix@gnome.org>
2023-06-18 22:34:02 +0200

    window: Bind offline-mode to rb-server

--- src/app.rs
+++ src/app.rs
@@ -290,11 +290,13 @@ impl SwApplication {
             *imp.rb_server.borrow_mut() = rb_server.clone();
             this.notify("rb-server");
 
-            window.enable_offline_mode(rb_server.is_none());
             imp.library.refresh_data();
 
-            if rb_server.is_some(){
+            if let Some(rb_server) = rb_server {
+                info!("Using radio-browser.info REST api: {rb_server}");
                 window.refresh_data();
+            }else{
+                warn!("Unable to find available radio-browser.info server.");
             }
         });
         spawn!(fut);
