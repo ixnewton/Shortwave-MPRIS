c0a2d9f89d787f6a4d271166035dfe20605272a2
Philipp Bartsch <phil@grmr.de>
2024-05-08 02:56:04 +0200

    Make clippy happy!
    https://rust-lang.github.io/rust-clippy/master/index.html#legacy_numeric_constants

--- src/audio/controller/mpris_controller.rs
+++ src/audio/controller/mpris_controller.rs
@@ -131,8 +131,7 @@ impl MprisController {
 
         // mpris volume
         self.mpris.connect_volume(clone!(@strong self.sender as sender, @weak self.volume as old_volume => move |new_volume| {
-            // if *old_volume.borrow() != new_volume {
-            if (*old_volume.borrow() - new_volume).abs() > std::f64::EPSILON {
+            if (*old_volume.borrow() - new_volume).abs() > f64::EPSILON {
                 send!(sender, Action::PlaybackSetVolume(new_volume));
                 *old_volume.borrow_mut() = new_volume;
             }
