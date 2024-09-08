2417d6cdf606ffb56cb50c388ba531ed79a27d88
Felix Häcker <haeckerfelix@gnome.org>
2024-09-08 14:40:52 +0200

    app: Drop action message passing mechanism

--- src/app.rs
+++ src/app.rs
@@ -15,28 +15,20 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use std::cell::{OnceCell, RefCell};
-use std::str::FromStr;
 
 use adw::prelude::*;
 use adw::subclass::prelude::*;
-use async_channel::{Receiver, Sender};
 use gio::subclass::prelude::ApplicationImpl;
 use glib::{clone, Properties};
 use gtk::glib::WeakRef;
 use gtk::{gio, glib};
 
-use crate::api::{SwClient, SwStationSorting};
+use crate::api::SwClient;
 use crate::audio::SwPlayer;
 use crate::config;
 use crate::database::SwLibrary;
-use crate::settings::{settings_manager, Key, SwSettingsDialog};
+use crate::settings::{settings_manager, SwSettingsDialog};
 use crate::ui::{about_dialog, SwApplicationWindow};
-
-#[derive(Debug, Clone)]
-pub enum Action {
-    // Audio Playback
-    SettingsKeyChanged(Key),
-}
 
 mod imp {
     use super::*;
@@ -51,9 +43,6 @@ mod imp {
         #[property(get)]
         pub rb_server: RefCell<Option<String>>,
 
-        pub sender: Sender<Action>,
-        pub receiver: RefCell<Option<Receiver<Action>>>,
-
         pub window: OnceCell<WeakRef<SwApplicationWindow>>,
         pub settings: gio::Settings,
     }
@@ -65,9 +54,6 @@ mod imp {
         type Type = super::SwApplication;
 
         fn new() -> Self {
-            let (sender, r) = async_channel::bounded(10);
-            let receiver = RefCell::new(Some(r));
-
             let library = SwLibrary::default();
             let player = SwPlayer::new();
             let rb_server = RefCell::default();
@@ -79,8 +65,6 @@ mod imp {
                 library,
                 player,
                 rb_server,
-                sender,
-                receiver,
                 window,
                 settings,
             }
@@ -111,18 +95,6 @@ mod imp {
             // Setup app level GActions
             app.setup_gactions();
 
-            // Setup action channel
-            let receiver = self.receiver.borrow_mut().take().unwrap();
-            glib::spawn_future_local(clone!(
-                #[weak]
-                app,
-                async move {
-                    while let Ok(action) = receiver.recv().await {
-                        app.process_action(action);
-                    }
-                }
-            ));
-
             // Find radiobrowser server and update library data
             let fut = clone!(
                 #[weak]
@@ -132,22 +104,6 @@ mod imp {
                 }
             );
             glib::spawn_future_local(fut);
-
-            // Setup settings signal (we get notified when a key gets changed)
-            self.settings.connect_changed(
-                None,
-                clone!(
-                    #[strong(rename_to = sender)]
-                    self.sender,
-                    move |_, key_str| {
-                        let key: Key = Key::from_str(key_str).unwrap();
-                        crate::utils::send(&sender, Action::SettingsKeyChanged(key));
-                    }
-                ),
-            );
-
-            // Small workaround to update every view to the correct sorting/order.
-            crate::utils::send(&self.sender, Action::SettingsKeyChanged(Key::ViewSorting));
         }
     }
 
@@ -232,38 +188,6 @@ impl SwApplication {
         self.set_accels_for_action("app.show-preferences", &["<primary>comma"]);
         self.set_accels_for_action("app.quit", &["<primary>q"]);
         self.set_accels_for_action("window.close", &["<primary>w"]);
-    }
-
-    fn process_action(&self, action: Action) -> glib::ControlFlow {
-        if self.active_window().is_none() {
-            return glib::ControlFlow::Continue;
-        }
-
-        match action {
-            Action::SettingsKeyChanged(key) => self.apply_settings_changes(key),
-        }
-        glib::ControlFlow::Continue
-    }
-
-    fn apply_settings_changes(&self, key: Key) {
-        debug!("Settings key changed: {:?}", &key);
-        match key {
-            Key::ViewSorting | Key::ViewOrder => {
-                let value = settings_manager::string(Key::ViewSorting);
-                let sorting = SwStationSorting::from_str(&value).unwrap();
-                let order = settings_manager::string(Key::ViewOrder);
-                let descending = order == "Descending";
-
-                self.imp()
-                    .window
-                    .get()
-                    .unwrap()
-                    .upgrade()
-                    .unwrap()
-                    .set_sorting(sorting, descending);
-            }
-            _ => (),
-        }
     }
 
     async fn lookup_rb_server(&self) {
