9a6898b6d10b78dac85f97c9e8b95c25bc75da29
Felix Häcker <haeckerfelix@gnome.org>
2024-09-08 22:47:56 +0200

    device: Drop mdns in favor of mdns-sd

--- src/device/device_discovery.rs
+++ src/device/device_discovery.rs
@@ -15,27 +15,23 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use std::cell::Cell;
-use std::collections::HashMap;
 use std::time::Duration;
 
 use adw::prelude::*;
 use async_io::Timer;
 use futures_lite::future::FutureExt;
-use futures_lite::StreamExt;
 use glib::subclass::prelude::*;
 use glib::{clone, Properties};
 use gtk::glib;
-use mdns::Error;
+use mdns_sd::{Error, ServiceDaemon, ServiceEvent};
 
 use super::{SwDevice, SwDeviceKind, SwDeviceModel};
 use crate::i18n::i18n;
 
 mod imp {
-    use futures_lite::pin;
-
     use super::*;
 
-    const CAST_SERVICE: &str = "_googlecast._tcp.local";
+    const CAST_SERVICE: &str = "_googlecast._tcp.local.";
 
     #[derive(Debug, Default, Properties)]
     #[properties(wrapper_type = super::SwDeviceDiscovery)]
@@ -69,27 +65,25 @@ mod imp {
 
     impl SwDeviceDiscovery {
         pub async fn discover_cast_devices(&self) -> Result<(), Error> {
-            let stream = mdns::discover::all(CAST_SERVICE, Duration::from_secs(3))?.listen();
-            pin!(stream);
+            let mdns = ServiceDaemon::new()?;
+            let receiver = mdns.browse(CAST_SERVICE)?;
 
-            while let Some(Ok(response)) = stream.next().await {
-                if let Some(addr) = response.ip_addr() {
-                    let txt_records: Vec<&str> = response.txt_records().collect();
-                    let mut values = HashMap::new();
-
-                    for value in txt_records {
-                        let parts: Vec<&str> = value.splitn(2, '=').collect();
-                        if parts.len() == 2 {
-                            values.insert(parts[0].to_string(), parts[1].to_string());
-                        }
-                    }
+            while let Ok(event) = receiver.recv_async().await {
+                if let ServiceEvent::ServiceResolved(info) = event {
+                    let host = info.get_addresses().iter().next().unwrap().to_string();
 
                     let device = SwDevice::new(
-                        values.get("id").unwrap_or(&addr.to_string()),
+                        info.get_property("id")
+                            .map(|txt| txt.val_str())
+                            .unwrap_or(&host),
                         SwDeviceKind::Cast,
-                        values.get("fn").unwrap_or(&i18n("Google Cast Device")),
-                        values.get("md").unwrap_or(&i18n("Unknown Model")),
-                        &addr.to_string(),
+                        info.get_property("fn")
+                            .map(|txt| txt.val_str())
+                            .unwrap_or(&i18n("Google Cast Device")),
+                        info.get_property("md")
+                            .map(|txt| txt.val_str())
+                            .unwrap_or(&i18n("Unknown Model")),
+                        &host,
                     );
                     self.devices.add_device(&device);
                 }
